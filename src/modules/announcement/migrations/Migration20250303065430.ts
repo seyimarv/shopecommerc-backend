28e30b34f7685484eac83ab227612df54446f4bc
Seyimarv <oluwaseyitan299@gmail.com>
2025-03-03 07:57:20 +0100

    update

---
+++ src/modules/announcement/migrations/Migration20250303065430.ts
@@ -0,0 +1,14 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250303065430 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`create table if not exists "announcement" ("id" text not null, "message" text not null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "announcement_pkey" primary key ("id"));`);
+    this.addSql(`CREATE INDEX IF NOT EXISTS "IDX_announcement_deleted_at" ON "announcement" (deleted_at) WHERE deleted_at IS NULL;`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`drop table if exists "announcement" cascade;`);
+  }
+
+}
